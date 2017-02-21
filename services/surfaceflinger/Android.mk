2ed5b671ec161d7c7abd6661d243f4e33f0852fc
Alex Naidis <alex.naidis@linux.com>
2017-02-21 17:21:17 +0100

    SF: Update COLOR_METADATA handling
    - Enable it for msm8996 automatically
      unless the target opts out.
    - Generally allow other targets to
      opt in.
    
    Change-Id: I6966e472c8cef404be5c924effc92a7f6bde8762
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -175,11 +175,16 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
   LOCAL_SHARED_LIBRARIES += libqdMetaData
   LOCAL_CFLAGS += -DQTI_BSP
 
-    ifeq ($(TARGET_SUPPORTS_COLOR_METADATA), true)
-      ifeq ($(call is-board-platform-in-list, msm8996), true)
-        LOCAL_CFLAGS += -DUSE_COLOR_METADATA
-      endif
+  ifeq ($(TARGET_SUPPORTS_COLOR_METADATA),)
+    ifeq ($(call is-board-platform-in-list, msm8996), true)
+      TARGET_SUPPORTS_COLOR_METADATA := true
     endif
+  endif
+
+  ifeq ($(TARGET_SUPPORTS_COLOR_METADATA), true)
+      LOCAL_CFLAGS += -DUSE_COLOR_METADATA
+  endif
+
 endif
 
 LOCAL_MODULE := libsurfaceflinger
