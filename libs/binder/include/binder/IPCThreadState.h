130b460e13258832ea7ce67dcb7d22ad10534c21
Martijn Coenen <maco@google.com>
2017-11-17 10:46:32 +0100

    Flush BC_FREE_BUFFER and ref ops from non-looper threads.
    BC_FREE_BUFFER and ref commands are normally just queued, and not
    automatically flushed out to the kernel driver. This usually works fine,
    because BC_FREE_BUFFER is typically called from a binder thread (which
    flushes when calling back into the kernel), or a thread making regular
    binder transactions itself.
    
    But it can happen that a Parcel is destructed from a thread that meets
    neither of those requirements; especially Parcels created from Java are
    sensitive to this, because if they aren't immediately recycled, they
    will instead be garbage collected, and in that case the BC_FREE_BUFFER
    will be queued to the FinalizerDaemon thread, which otherwise never
    makes or receives any binder calls.
    
    To prevent these commands from getting stuck, flush BC_FREE_BUFFER and
    refcount operations automatically from such threads.
    
    Bug: 68604253
    Bug: 139697085
    Test: boots, binderLibTest
    Change-Id: I98109a7046c122db22af0b15a268629284f06663
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- libs/binder/include/binder/IPCThreadState.h
+++ libs/binder/include/binder/IPCThreadState.h
@@ -92,6 +92,7 @@ public:
             int                 setupPolling(int* fd);
             status_t            handlePolledCommands();
             void                flushCommands();
+            bool                flushIfNeeded();
 
             void                joinThreadPool(bool isMain = true);
             
@@ -188,6 +189,7 @@ private:
             int32_t             mWorkSource;
             // Whether the work source should be propagated.
             bool                mPropagateWorkSource;
+            bool                mIsLooper;
             int32_t             mStrictModePolicy;
             int32_t             mLastTransactionBinderFlags;
 
