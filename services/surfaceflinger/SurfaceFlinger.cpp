0bdbf5fdcbf82cb9cc2420fe6674df716d874616
Robert Carr <racarr@google.com>
2019-02-19 10:05:00 -0800

    [RESTRICT AUTOMERGE]: Exclude secure layers from most screenshots taken by the system server.
    In pre-P versions of Android, it was allowed to screenshot secure layers if the
    buffer queue producer which was the target of the screenshot was owned by
    the system (in this case SurfaceFlinger). This really was a synonym for:
    The screen rotation animation was allowed to capture secure layers, but the other
    code paths weren't. In O we mistakenly changed this check to always allow the system server
    to capture secure layers via the captureScreen path (the captureLayers path used for
    TaskSnapshots was unaffected). This can result in data leakage in cases where the
    system server takes screenshots on behalf of other parts of the system (e.g. for
    the assistant). To mitigate this we provide an explicit switch for the system server
    to specify whether it wishes to capture Secure layers. While this is dangerous, I think
    it is less dangerous than the previous implicit switch of capturing secure layers based on which
    type of BufferQueue was passed in. The flag defaults to not capturing secure layers
    and we set it to true in the one place we need it (for the screen rotation animation).
    Non privileged clients can still not capture secure layers at all directly.
    
    Test: SetFlagsSecureEUidSystem
    Bug: 120610669
    Change-Id: I288ad3bbb0444306e90fe3bb15e51b447539dea5
    (cherry picked from commit 5c99901e77437e403ba643c9dc839ee9659acada)

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -4884,7 +4884,8 @@ status_t SurfaceFlinger::captureScreen(const sp<IBinder>& display, sp<GraphicBuf
                                        Rect sourceCrop, uint32_t reqWidth, uint32_t reqHeight,
                                        int32_t minLayerZ, int32_t maxLayerZ,
                                        bool useIdentityTransform,
-                                       ISurfaceComposer::Rotation rotation) {
+                                       ISurfaceComposer::Rotation rotation,
+                                       bool captureSecureLayers) {
     ATRACE_CALL();
 
     if (CC_UNLIKELY(display == 0)) return BAD_VALUE;
@@ -4906,7 +4907,8 @@ status_t SurfaceFlinger::captureScreen(const sp<IBinder>& display, sp<GraphicBuf
         }
     }
 
-    DisplayRenderArea renderArea(device, sourceCrop, reqWidth, reqHeight, renderAreaRotation);
+    DisplayRenderArea renderArea(device, sourceCrop, reqWidth, reqHeight, renderAreaRotation,
+                                 captureSecureLayers);
 
     auto traverseLayers = std::bind(std::mem_fn(&SurfaceFlinger::traverseLayersInDisplay), this,
                                     device, minLayerZ, maxLayerZ, std::placeholders::_1);
