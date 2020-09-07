607f844bd3f013440106b7d3c46fe0710e73a2f9
KaiChieh Chuang <kaichieh.chuang@mediatek.com>
2020-09-07 13:48:42 +0800

    [RenderEngine] Introduce non-linear display color transform.
    A display color transform is a non-linear color matrix that should be
    applied in gamma space. Previously the non-linear display color transform
    is mixed into the linear color matrix that results in incorrect color
    inversion behaviour.
    
    RenderEngineTest
    fix fillBufferColorTransform calling wrong function
    fillBufferColorTransform display color transform set to 0.9f
    add renderengine object for useColorManagement enabled,
    to test color management affected on display and layer color transform
    
    Bug: 157203983
    Test: color inversion
    Test: run RenderEngineTest
    Change-Id: Ic731f014a14795b80323eeaf929761c828150f91
    Merged-In: Ic731f014a14795b80323eeaf929761c828150f91
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- libs/renderengine/gl/GLESRenderEngine.cpp
+++ libs/renderengine/gl/GLESRenderEngine.cpp
@@ -1102,6 +1102,7 @@ status_t GLESRenderEngine::drawLayers(const DisplaySettings& display,
 
     setOutputDataSpace(display.outputDataspace);
     setDisplayMaxLuminance(display.maxLuminance);
+    setDisplayColorTransform(display.colorTransform);
 
     const mat4 projectionMatrix =
             ui::Transform(display.orientation).asMatrix4() * mState.projectionMatrix;
@@ -1171,7 +1172,7 @@ status_t GLESRenderEngine::drawLayers(const DisplaySettings& display,
         position[3] = vec2(bounds.right, bounds.top);
 
         setupLayerCropping(*layer, mesh);
-        setColorTransform(display.colorTransform * layer->colorTransform);
+        setColorTransform(layer->colorTransform);
 
         bool usePremultipliedAlpha = true;
         bool disableTexture = true;
@@ -1325,6 +1326,10 @@ void GLESRenderEngine::setupLayerTexturing(const Texture& texture) {
 
 void GLESRenderEngine::setColorTransform(const mat4& colorTransform) {
     mState.colorMatrix = colorTransform;
+}
+
+void GLESRenderEngine::setDisplayColorTransform(const mat4& colorTransform) {
+    mState.displayColorMatrix = colorTransform;
 }
 
 void GLESRenderEngine::disableTexturing() {
